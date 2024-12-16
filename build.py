d53daa43242d534e4cecadafe3eb7b75ca2fef09
BB <me@yomama.com>
2024-12-16 10:28:09 +0300

    -

--- build.py
+++ build.py
@@ -1,17 +1,20 @@
 import os, sys
 
 if len(sys.argv) == 1 or sys.argv[1] == 'build-lewi':
-    os.system("javac -classpath jlewi/src/java/:jlewi/lib/commons-lang3-3.13.0.jar jlewi/src/java/swisseph/*.java")
+    os.system("javac -source 8 -target 8 -classpath jlewi/src/java/:jlewi/lib/commons-lang3-3.13.0.jar jlewi/src/java/swisseph/*.java")
     os.system("javac -classpath jlewi/src/java/:jlewi/lib/commons-lang3-3.13.0.jar jlewi/src/java/org/jlewi/*.java")
     
 if len(sys.argv) == 1 or sys.argv[1] == 'jar':
-    os.system("jar cvf astromaestro.jar -C jlewi/src/java .")
+    os.system("jar cmvf META-INF/MANIFEST.MF astromaestro.jar -C jlewi/src/java .")
 
 if len(sys.argv) == 1 or sys.argv[1] == 'gen-decans':
     os.system("java -classpath jlewi/lib/commons-lang3-3.13.0.jar:astromaestro.jar org.jlewi.GenerateDecans")
 
 if len(sys.argv) == 1 or sys.argv[1] == 'test-vedic':
     os.system("java -classpath jlewi/lib/commons-lang3-3.13.0.jar:astromaestro.jar swisseph.VedicTest")
+
+if len(sys.argv) == 1 or sys.argv[1] == 'test-vedic2':
+    os.system("java -classpath jlewi/lib/commons-lang3-3.13.0.jar:astromaestro.jar swisseph.Vedic")
 
 if len(sys.argv) == 1 or sys.argv[1] == 'gen-combined':
     import astromaestro
