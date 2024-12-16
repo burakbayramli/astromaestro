bbd1103d11b339d6ecb0b7edca842616e9bc9c3e
BB <me@yomama.com>
2024-12-16 12:06:46 +0300

    -

--- web/app.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from flask import Flask
-
-app = Flask(__name__)
-        
-if __name__ == '__main__':
-    app.debug = True
-    app.secret_key = "secretkeyverysecret" # needed for session[] to work
-    app.run(host="localhost",port=5000)
