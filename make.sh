e8215f59bdf0ea7cc069a7c4dd3654fe44cf0e5c
BB <me@yomama.com>
2025-04-15 12:07:53 +0300

    -

--- make.sh
+++ make.sh
@@ -1,2 +1,3 @@
+rm /tmp/astclasses/*.class
 javac -d /tmp/astclasses -classpath jlewi/src/java/:jlewi/lib/commons-lang3-3.13.0.jar Combined.java
 java -classpath /tmp/astclasses:jlewi/lib/commons-lang3-3.13.0.jar VedicTest
