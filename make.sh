2aef687ff0b68f5655ff8bc20b676313f1953150
BB <me@yomama.com>
2025-04-15 13:29:56 +0300

    -

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 rm /tmp/astclasses/*.class
 javac -d /tmp/astclasses -classpath jlewi/src/java/:jlewi/lib/commons-lang3-3.13.0.jar Combined3.java
 java -classpath /tmp/astclasses:jlewi/lib/commons-lang3-3.13.0.jar VedicTest
+java -classpath /tmp/astclasses:jlewi/lib/commons-lang3-3.13.0.jar DecanTest
